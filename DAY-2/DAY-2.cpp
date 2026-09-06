1ea430c0a830c6240b0d3f57d961b57113347df4
Abhirup Kovid <abhirupkovid@gmail.com>
2026-09-06 10:53:53 +0530

    Day 2 completed

---
+++ DAY-2/DAY-2.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n = 10;
+    if(n%2==0){
+        cout<<"The number is even"<<endl;
+    }
+    else{
+        cout<<"The number is odd"<<endl;
+    }
+
+    //LOOPS
+    for(int i=0; i<10; i++){
+        cout<<i<<endl;
+    }
+
+    return 0;
+}
+
